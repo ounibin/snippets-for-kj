4f482ce66735e3da82a8cf6fade5141dbab461af
wangkj <>
2024-08-15 19:07:24 +0800

    新增渠道数据

---
+++ 160/channel_id.js
@@ -0,0 +1,20 @@
+export const CHANNEL_ID = {
+  NY_PC: '1', //PC端
+  // 患者端
+  PATIENT_BROWSER: '16',
+  PATIENT_WEIXIN: '23',
+  PATIENT_IOS: '24',
+  PATIENT_ANDROID: '20',
+  // 医护端
+  DOCTOR_IOS: '100000002',
+  DOCTOR_ANDROID: '100000001',
+  // 外部合作
+  MYCHANGSHA_APP: '100013549', // 我的长沙APP
+  MYCHANGSHA_WEIXIN: '100013553', // 我的长沙 微信
+  BANAN: '100013563', // 重庆-巴南区
+  SHINAN: '100013571', // 青岛-市南区
+  FOSHAN: '100013599', // 广东-佛山
+
+  // 运营后台
+  ADMIN_PC: '100011324', //运营后台
+}
